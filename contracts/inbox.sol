700dcb003181cd477a35fb7a87cd0db6b8d71dfc
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-08 18:57:28 +0330

    added inbox.sol to my contracts directory

---
+++ contracts/inbox.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.17;
+
+
+contract Inbox {
+    
+    string public message;
+    
+    function Inbox(string initailMessage) public {
+        message = initailMessage;    
+    }
+    
+    function setMessage(string newMassage) public {
+        message = newMassage;
+    }
+
+}
