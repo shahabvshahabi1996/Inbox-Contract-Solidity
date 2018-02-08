d58bbfdc4e81a141e88eb58f4afe425e4eb8c335
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-09 00:03:42 +0330

    added provider and fix bugs

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider()); //this porvider is changable
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const {interface , bytecode} = require('../compile');
 
 let accounts;
@@ -16,6 +17,7 @@ beforeEach(async ()=>{
          data : bytecode ,
          arguments : ['Hi there!']
         }).send({ from : accounts[0], gas : '1000000' });
+    inbox.setProvider(provider);    
 });
 
 describe("Inbox",()=>{
