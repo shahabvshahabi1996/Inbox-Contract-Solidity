9e9fade0b279bf90795a60dbb5f6755991b78588
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2018-02-09 00:28:00 +0330

    added test method for setMessage contract method

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -28,5 +28,11 @@ describe("Inbox",()=>{
     it('has a default message', async() => {
          const message = await inbox.methods.message().call();
          assert.equal(message,'Hi there!');
-    })
+    });
+
+    it('can change the message',async()=>{
+        await inbox.methods.setMessage("Goodbye!").send({ from : accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message,"Goodbye!");
+    });
 })
